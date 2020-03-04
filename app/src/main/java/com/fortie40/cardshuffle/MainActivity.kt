65c77b29cc3725358aacb00887c2a766adc28d45
0________4 <you@example.com>
2020-03-04 11:59:10 +0300

    Add helper functions in MainActivity

--- app/src/main/java/com/fortie40/cardshuffle/MainActivity.kt
+++ app/src/main/java/com/fortie40/cardshuffle/MainActivity.kt
@@ -5,8 +5,10 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,5 +16,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+
+        calculate.setOnClickListener {
+            showWorkInProgress()
+        }
+
+        cancel.setOnClickListener {
+            showWorkIsFinished()
+        }
+    }
+
+    private fun showWorkInProgress() {
+        calculate.isEnabled = false
+        cancel.isEnabled = true
+        progress_bar.visibility = View.VISIBLE
+    }
+
+    private fun showWorkIsFinished() {
+        calculate.isEnabled = true
+        cancel.isEnabled = false
+        progress_bar.visibility = View.GONE
     }
 }
